8892bf58df828313eafb2db9fce69e2b4d4dbb78
Sebastian211-alt <ffranshesco119@gmail.com>
2022-03-30 09:42:07 -0600

    Se sube Proyecto6

---
+++ src/main/java/Clase6/Clase6.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clase6;
+
+/**
+ *
+ * @author SebasTati
+ */
+public class Clase6 {
+
+    public static void main(String[] args) {
+
+        int c = 0, num, cp = 0;
+
+        /*num = 6;
+
+        for (int j = 1; j <= 6; j++) {
+            if (num % j == 0) {
+                c++;
+            }
+        }
+
+        if (c == 2) {
+            System.out.println("ES PRIMO");
+        } else {
+            System.out.println("NO ES PRIMO"); 
+        }*/
+        int cnp = 0;
+        System.out.println("Ciclo For");
+
+        for (int i = 1; i <= 100; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    c++;
+                }
+
+                if (c == 2) {
+                    cp++;
+                } else {
+                    cnp++;
+
+                    System.out.println(" " + c);
+                    c = 0;
+                }
+            }
+            System.out.println();
+            System.out.println("Total primos: " + cp++);
+            System.out.println("Total no primos: " + cnp++);
+
+        }
+    }
+
+}
